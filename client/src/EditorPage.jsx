e64772f9d4af65a4df0850c5b3890357df0d4594
Nihal-patel9 <1999nihalpatel@gmail.com>
2025-03-03 20:28:22 +0530

    adding editor page

--- client/src/EditorPage.jsx
+++ client/src/EditorPage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { io } from 'socket.io-client';
+import { useParams } from 'react-router-dom';
 import './App.css';
 
 function EditorPage() {
+
+    const { id: documentId } = useParams();
+    const [value, setValue] = useState('');
+    const socketRef = useRef();
+
+    useEffect(() => {
+        const socket = io('http://localhost:5000');
+        socketRef.current = socket;
+
+        socket.emit('get-document', documentId);
+
+        socket.on('load-document', (document) => {
+            setValue(document);
+        });
+
+        socket.on('receive-changes', (delta) => {
+            setValue(delta);
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+    }, [documentId]);
+
+    const handleChange = (content, delta, source, editor) => {
+        setValue(content);
+        if (source === 'user') {
+            socketRef.current.emit('send-changes', content);
+        }
+    };
+
+    const handleShare = () => {
+        const url = `${window.location.origin}/document/${documentId}`;
+        navigator.clipboard.writeText(url);
+        alert(`Share this URL: ${url}`);
+    };
 
     const modules = {
         toolbar: [
@@ -27,11 +65,11 @@ function EditorPage() {
                     <h1>Untitled document</h1>
                 </div>
                 <div className="navbar-right">
-                    <button className="share-button">Share</button>
+                    <button className="share-button" onClick={handleShare} >Share</button>
                 </div>
             </div>
             <div className="toolbar-container">
-                <ReactQuill modules={modules} />
+                <ReactQuill modules={modules} value={value} onChange={handleChange} />
             </div>
         </div>
     )
