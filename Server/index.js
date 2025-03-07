57a0160bbe76b29c6c4fc40f7c8b1b0450c8ba8f
Nihal-patel9 <1999nihalpatel@gmail.com>
2025-03-07 20:21:35 +0530

    backend setup

---
+++ Server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+
+
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
