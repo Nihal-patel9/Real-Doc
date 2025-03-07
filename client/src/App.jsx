bd332bf31a6deda500541275db954217979b6441
Nihal-patel9 <1999nihalpatel@gmail.com>
2025-03-07 19:51:32 +0530

    Register page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import './App.css'
 import EditorPage from './EditorPage'
+import RegisterPage from './RegisterPage';
 
 function App() {
+
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  const router = createBrowserRouter([
+    {
+      path: '/register',
+      element: <><RegisterPage /></>
+    },
+    {
+      path: '/document/:id',
+      element: <>{isAuthenticated ? <EditorPage /> : <Navigate to="/login" />}</>
+    },
+    {
+      path: '*',
+      element: <><Navigate to={`/document/${Math.random().toString(36).substr(2, 9)}`} /></>
+    }
+  ])
 
 
   return (
     <>
-      <EditorPage />
+      < RouterProvider router={router} />
     </>
   )
 }
